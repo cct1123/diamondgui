d15862e396e7c2fb213e5c881fa41713cd3ecb8e
ChunTung Cheung <ctcheung1123@gmail.com>
2024-10-02 19:25:09 -0500

    update hardware config

--- hardware/config.py
+++ hardware/config.py
@@ -13,7 +13,7 @@ PS_chmap = {"laser":0,
             "dtrig":3,
             "mwA":4, 
             "mwB":5,
-            "phB":8
+            # "phB":8
             }
 
 PS_choffs = {"laser":0, 
@@ -21,47 +21,49 @@ PS_choffs = {"laser":0,
             "dtrig":0,
             "mwA":0, 
             "mwB":0,
-            "phB":0
+            # "phB":0
             }
 
 # AO Channels
 # please refer to the following for AO calibration
 # https://www.swabianinstruments.com/static/documentation/PulseStreamer/sections/api-doc.html#calibrating-the-analog-outputs 
-PS_aoSlope = 1.0
-PS_aoOffset = 0.0 # [V]
-PSaoch_PhaseShifter = 0 # AO channel for MW phase shifter which controls the phase of the MW line
-PSaoch_Attenuator = 1 # AO channel for the MW attenuator which controls the MW power
 
 # ------------------------------------------------------------------------------------------------
 # NI DAQ Settings --------------------------------------------------------------------------------
-DAQch_APD = "/Dev1/ai16"
-DAQch_Clock = "/Dev1/PFI8" # clock source
-DAQch_Trig = "/Dev1/PFI9" # trigger source
-DAQch_VDISynTrigOut = "/Dev1/port0/line7"
-DAQch_ZBD = "/Dev1/ai20"
+NI_ch_APD = "/Dev1/ai16"
+NI_ch_Clock = "/Dev1/PFI11" # clock source
+NI_ch_Trig = "/Dev1/PFI12" # trigger source
+NI_ch_VDISynTrigOut = "/Dev1/port0/line7"
+NI_ch_ZBD = "/Dev1/ai7"
+NI_ch_UCA = "/Dev1/ao1"
+NI_ch_MWBP = "/Dev1/ao2"
 
 # # for positioner I/O
-# DAQch_Ax1_InAp = "Dev1/port/line8"
-# DAQch_Ax1_InBp = "Dev1/port/line9"
-# DAQch_Ax2_InAp = "Dev1/port/line10"
-# DAQch_Ax2_InBp = "Dev1/port/line11"
-# DAQch_Ax3_InAp = "Dev1/port/line12"
-# DAQch_Ax3_InBp = "Dev1/port/line13"
+# NI_ch_Ax1_InAp = "Dev1/port/line8"
+# NI_ch_Ax1_InBp = "Dev1/port/line9"
+# NI_ch_Ax2_InAp = "Dev1/port/line10"
+# NI_ch_Ax2_InBp = "Dev1/port/line11"
+# NI_ch_Ax3_InAp = "Dev1/port/line12"
+# NI_ch_Ax3_InBp = "Dev1/port/line13"
 
-# DAQch_Ax1_OutAp = "Dev1/port/line14"
-# DAQch_Ax1_OutBp = "Dev1/port/line15"
-# DAQch_Ax2_OutBp = "Dev1/port/line16"
-# DAQch_Ax2_OutAp = "Dev1/port/line17"
-# DAQch_Ax3_OutAp = "Dev1/port/line18"
-# DAQch_Ax3_OutBp = "Dev1/port/line19"
-DAQch_Ax123_InABp = "/Dev1/port/line8:13"
-DAQch_Ax123_OutABp = "/Dev1/port/line14:19"
+# NI_ch_Ax1_OutAp = "Dev1/port/line14"
+# NI_ch_Ax1_OutBp = "Dev1/port/line15"
+# NI_ch_Ax2_OutBp = "Dev1/port/line16"
+# NI_ch_Ax2_OutAp = "Dev1/port/line17"
+# NI_ch_Ax3_OutAp = "Dev1/port/line18"
+# NI_ch_Ax3_OutBp = "Dev1/port/line19"
+NI_ch_Ax123_InABp = "/Dev1/port/line8:13"
+NI_ch_Ax123_OutABp = "/Dev1/port/line14:19"
 
-DAQchmap = dict(apd = DAQch_APD, 
-                clock = DAQch_Clock, 
-                trig = DAQch_Trig, 
-                pax123_in = DAQch_Ax123_InABp, 
-                pax123_out = DAQch_Ax123_OutABp)
+NI_chmap = dict(apd = NI_ch_APD, 
+                clock = NI_ch_Clock, 
+                trig = NI_ch_Trig, 
+                pax123_in = NI_ch_Ax123_InABp, 
+                pax123_out = NI_ch_Ax123_OutABp)
+
+NI_ratebase = 100.0E6 # Hz
+NI_timebase = int(1.0/NI_ratebase*1E9) #ns
+NI_sampling_max = 500E3 # Hz
 # # 
 
 # ------------------------------------------------------------------------------------------------
@@ -84,3 +86,4 @@ LASER_SN = "LAS-09434" # default ip
 VDISYN_SN = "VDIS200A" # VDI synthesizer serial number
 VDISYN_VIDPID = "0403:6001" # the USB VID:PID
 VDISYN_BAUD = 921600 # USB baud rate between PC and VDI sythesizer
+VDISYN_timebase = int(4) #ns
