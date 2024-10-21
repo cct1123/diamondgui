7f0a4bbe73cb732fad65f0de3fdfd588a083a8d6
ChunTung Cheung <ctcheung1123@gmail.com>
2024-10-21 18:40:34 -0500

    add example of drag-and-drop

---
+++ app/examples/clientcallback.py
@@ -0,0 +1,144 @@
+from dash import Dash, dcc, html, Input, Output, clientside_callback, callback
+import pandas as pd
+import json
+
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+available_countries = df['country'].unique()
+
+
+graph_layout = html.Div([
+    dcc.Graph(
+        id='clientside-graph-px'
+    ),
+    dcc.Store(
+        id='clientside-figure-store-px'
+    ),
+    'Indicator',
+    dcc.Dropdown(
+        {'pop' : 'Population', 'lifeExp': 'Life Expectancy', 'gdpPercap': 'GDP per Capita'},
+        'pop',
+        id='clientside-graph-indicator-px'
+    ),
+    'Country',
+    dcc.Dropdown(available_countries, 'Canada', id='clientside-graph-country-px'),
+    'Graph scale',
+    dcc.RadioItems(
+        ['linear', 'log'],
+        'linear',
+        id='clientside-graph-scale-px'
+    ),
+    html.Hr(),
+    html.Details([
+        html.Summary('Contents of figure storage'),
+        dcc.Markdown(
+            id='clientside-figure-json-px'
+        )
+    ])
+])
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        dcc.Store(id="notification-permission"),
+        html.Button("Notify", id="notify-btn"),
+        html.Div(id="notification-output"),
+        graph_layout
+    ]
+)
+
+
+
+
+@callback(
+    Output('clientside-figure-store-px', 'data'),
+    Input('clientside-graph-indicator-px', 'value'),
+    Input('clientside-graph-country-px', 'value')
+)
+def update_store_data(indicator, country):
+    dff = df[df['country'] == country]
+    return px.scatter(dff, x='year', y=str(indicator))
+
+
+clientside_callback(
+    """
+    function(figure, scale) {
+        if(figure === undefined) {
+            return {'data': [], 'layout': {}};
+        }
+        const fig = Object.assign({}, figure, {
+            'layout': {
+                ...figure.layout,
+                'yaxis': {
+                    ...figure.layout.yaxis, type: scale
+                }
+             }
+        });
+        return fig;
+    }
+    """,
+    Output('clientside-graph-px', 'figure'),
+    Input('clientside-figure-store-px', 'data'),
+    Input('clientside-graph-scale-px', 'value')
+)
+
+
+@callback(
+    Output('clientside-figure-json-px', 'children'),
+    Input('clientside-figure-store-px', 'data')
+)
+def generated_px_figure_json(data):
+    return '```\n'+json.dumps(data, indent=2)+'\n```'
+
+
+
+
+
+
+clientside_callback(
+    """
+    function() {
+        return navigator.permissions.query({name:'notifications'})
+    }
+    """,
+    Output("notification-permission", "data"),
+    Input("notify-btn", "n_clicks"),
+    prevent_initial_call=True,
+)
+
+clientside_callback(
+    """
+    function(result) {
+        if (result.state == 'granted') {
+            new Notification("Dash notification", { body: "Notification already granted!"});
+            return null;
+        } else if (result.state == 'prompt') {
+            return new Promise((resolve, reject) => {
+                Notification.requestPermission().then(res => {
+                    if (res == 'granted') {
+                        new Notification("Dash notification", { body: "Notification granted!"});
+                        resolve();
+                    } else {
+                        reject(`Permission not granted: ${res}`)
+                    }
+                })
+            });
+        } else {
+            return result.state;
+        }
+    }
+    """,
+    Output("notification-output", "children"),
+    Input("notification-permission", "data"),
+    prevent_initial_call=True,
+)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/examples/drag_and_drop.py
@@ -0,0 +1,115 @@
+import dash
+from dash.dependencies import Input, Output, ClientsideFunction, State
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+app = dash.Dash(
+    __name__,
+    external_scripts=["https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.js"],
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+ID = "drag_and_drop-"
+INTERVAL_DRAG_ORDER = 1000
+
+
+layout_hidden = dbc.Row([
+    dcc.Interval(id=ID+'interval-uppdate', interval=INTERVAL_DRAG_ORDER, n_intervals=0), #ms
+])
+
+layout_orderlabel = dbc.Row([
+    html.Div(
+        id=ID+"order",
+        children=[]
+    ),
+], style={"margin-top": "auto", "margin-bottom": "auto", "margin-left": "auto", "margin-right": "auto"})
+
+layout_draggablecontainer = dbc.Row([
+    html.Div(
+        id=ID+"drag_container",
+        className="container",
+        # className="bs container row",
+        style={
+            "display": "flex",
+            "overflow-x": "auto",
+            "overflow-y": "hidden",
+            "margin-left": "auto",
+            "margin-right": "auto",
+            "width": "100%"
+        },
+        children=[
+            dbc.Card([
+                dbc.CardHeader(f"Card {i}"),
+                dbc.CardImg(src="https://cdn-icons-png.flaticon.com/512/3304/3304942.png",
+                            top=True,
+                            ),
+                dbc.CardBody(
+                    dbc.Col([
+                        dbc.Row([
+                            dbc.Col([
+                                dbc.Button(
+                                    "Pause", outline=True, color="warning",
+                                    id=ID+f"button-pause-{i}", n_clicks=0
+                                ),
+                            ], width=2),
+                            dbc.Col([
+                                html.Div(id=ID+f"status-{i}")
+                            ], width=8)
+                        ]),
+                        dbc.Row(children=[
+                            dbc.Col([dbc.Progress(
+                                value=0.5, min=0.0, max=1.0, id=ID + "-progressbar", animated=True, striped=True, label="",
+                                color="info", className="mt-2 mb-2"
+                            ),])
+                        ]),
+                    ]),
+                ),
+            ], 
+            id=ID+f"child-{i}",
+            style={
+                "display": "inline-block",
+                "width": "18rem",
+                "margin-left": "0.2em",
+                "margin-right": "0.2em"
+            }) for i in range(6)
+        ],
+    ),
+])
+
+app.layout = dbc.Col(
+    id="main",
+    children=[
+        layout_hidden,
+        layout_draggablecontainer,
+        layout_orderlabel,
+    ],
+)
+
+app.clientside_callback(
+    ClientsideFunction(namespace="clientside", function_name="make_draggable"),
+    Output(ID+"drag_container", "data-drag"),
+    [Input(ID+"drag_container", "id")],
+    [State(ID+"drag_container", "children")],
+)
+
+
+@app.callback(
+    Output(ID+"order", "children"),
+    [   Input(ID+"interval-uppdate", "n_intervals"),
+        Input(ID+"drag_container", component_property="children"),
+    ],
+)
+def watch_children(_n_intervals, children):
+    """Display on screen the order of children"""
+    dict_order = {}
+    label_children = []
+    for (ii, comp) in enumerate(children):
+        comp_id = comp["props"]["id"]
+        dict_order[comp["props"]["id"]] = ii
+        label = f"{comp_id}: {ii}-th order!!"
+        label_children.append(
+             dbc.Alert(label, color="info"),
+        )
+    return label_children
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
